b68271645086e5f2adc1b09cdf5e51fcd84b8f0d
Janderson <jandersonlucasribeiro@gmail.com>
2025-01-17 11:51:12 -0300

    Atualização no jogo

--- iniciante-em-programacao/logica-js-projeto_inicial/app.js
+++ iniciante-em-programacao/logica-js-projeto_inicial/app.js
@@ -1,8 +1,12 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 5;
+let numeroSecreto = 2;
+console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 30');
 
+// Se chute for igual ao número secreto
 if (numeroSecreto == chute) {
-    console.log('Isso aí! Você descobriu o número secreto (5)');
+    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
 
+} else {
+    alert('Você errou :(')
 }
